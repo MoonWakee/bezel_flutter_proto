f712fa8327cbb3c2f7fb61cef7d6a86a8a426a60
harry <jl5271@columbia.edu>
2023-07-10 22:45:43 -0400

    cloned, start off with navigation

---
+++ lib/screens/navigator/navigator_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NavigatorScreen extends StatelessWidget {
+  static String routeName = "/navigate";
+
+  const NavigatorScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
